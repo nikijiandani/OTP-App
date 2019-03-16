9ef2b9dd9c4c5b3b4d0295571a0731d384d59994
Nikita Jiandani <niki.jiandani@gmail.com>
2019-03-16 07:09:54 -0400

    Add serving the 'public' directory section

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('This is an OTP-App!'));
 
+//SERVING THE PUBLIC DIRECTORY
+app.use(express.static(__dirname + "/public"));
+
 app.listen(port, () => console.log(`OTP app listening on port ${port}!`));
